1848d0aeb6f0cafe26ef09628550707c94da244c
AnakUtara <141168468+AnakUtara@users.noreply.github.com>
2025-01-08 22:48:21 +0700

    fix(mouse-look-x): fix clamp implementation from incremental behavior of Rotate to overwriting value per frame with localRotation

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,9 +1,11 @@
+using Unity.Mathematics.Geometry;
 using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
     [SerializeField] private float sensitivity = 100f;
     [SerializeField] private Transform playerBody;
+    private float xRotation = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +18,8 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
         playerBody.Rotate(0f, mouseX, 0f);
-        transform.Rotate(-Mathf.Clamp(mouseY, -90f, 90f), 0f, 0f);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }   
 }
