3d244b22ef19bca6e3ab38a5ac62403bb203a100
AnakUtara <141168468+AnakUtara@users.noreply.github.com>
2025-01-08 23:15:45 +0700

    feat(wasd-movement): finish basic wasd movement implementation with Character Controller. No gravity yet

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] CharacterController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * (moveSpeed * Time.deltaTime));
+    }
+}
