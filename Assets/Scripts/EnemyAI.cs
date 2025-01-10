ea2f2d86b0655bf160aa7163ade6934797ef656c
AnakUtara <141168468+AnakUtara@users.noreply.github.com>
2025-01-10 15:47:28 +0700

    feat(chase-ai): add initial enemy chase ai navigation implementation

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float chaseRadius;
+    private float _distanceToTarget;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // void Start()
+    // {
+    //     
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (_distanceToTarget <= chaseRadius)
+        {
+            agent.isStopped = true;
+        }
+        else
+        {
+            agent.isStopped = false;
+            agent.SetDestination(target.position);
+        }
+    }
+}
